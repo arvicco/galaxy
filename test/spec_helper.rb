c85ec8cc697ae4077b64b3391f6064ddb261ea6a
arvicco <arvitallian@gmail.com>
2010-03-11 14:50:41 +0300

    Cleanup 2010-03-11 14:50:38

--- test/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'galaxy'
-#require 'spec'
-require 'spec/test/unit'
-#
-#Spec::Runner.configure do |config|
-#
-#end
